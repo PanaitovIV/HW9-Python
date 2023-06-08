2f2ea1e111b2d3b77ef97547a96c70eb1a5acef8
Ivan Panaitov <PanaitovIV@gmail.com>
2023-06-08 15:10:23 +0300

    добавили задание первое

---
+++ task_1/program.py
@@ -0,0 +1,11 @@
+# Работать с файлом california_housing_train.csv, который находится в папке sample_data. 
+# Определить среднюю стоимость дома, где кол-во людей от 0 до 500 (population).
+
+
+import pandas as pd
+
+df = pd.read_csv('task_1/california_housing_train.csv')
+filtered_df = df[(df['population'] >= 0) & (df['population'] <= 500)]
+print(filtered_df.head(10))
+mean_house_valie = filtered_df['median_house_value'].mean()
+print('Средняя стоимость дома, где количество людей от 0 до 500 > ', mean_house_valie)
